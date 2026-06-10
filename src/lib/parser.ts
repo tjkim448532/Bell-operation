3d89a13926e28421526b93c401b0b5ac49de896f
tjkim448532 <tjkim448532@github.com>
2026-06-10 12:00:33 +0900

    fix: 엑티비티에 놀이동산/모토아레나가 임의로 강제 합산되는 오류 수정

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -80,7 +80,7 @@ export async function parseRevenueBuffer(buffer: Buffer, filename: string, teamM
         // Fallbacks
         if (colName.includes('목장') || colName.includes('얼룩말카페')) team = '목장';
         else if (colName.includes('미디어아트센터') || colName.includes('기프트샵') || colName.includes('미디어아트센터 카페')) team = '미디어아트센터';
-        else if (['마운틴카트', '사계절썰매장', '놀이동산', '놀이동산(2025)', '모토아레나'].includes(colName)) team = '엑티비티';
+        else if (['마운틴카트', '사계절썰매장'].includes(colName)) team = '엑티비티';
       }
 
       if (teamSums[team] !== undefined) {
